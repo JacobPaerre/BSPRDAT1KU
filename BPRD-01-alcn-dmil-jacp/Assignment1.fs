64129ca301ef9445f60aef833cd5c3e1c3c14df0
Jacob Pærregaard <jacp@itu.dk>
2024-08-29 11:50:25 +0200

    completed assignment 1.2

--- BPRD-01-alcn-dmil-jacp/Assignment1.fs
+++ BPRD-01-alcn-dmil-jacp/Assignment1.fs
@@ -59,3 +59,58 @@ module Assignment1 =
             
     let e5 = If(Var "a", CstI 11, CstI 22)
     
+    // Exercise 1.2 (i)
+    type aexpr = 
+      | CstI of int
+      | Var of string
+      | Add of aexpr * aexpr
+      | Mul of aexpr * aexpr
+      | Sub of aexpr * aexpr
+      
+    // Exercise 1.2 (ii)
+    let aexpr1 = Sub(Var "v", Add(Var "w", Var "z"))
+    let aexpr2 = Mul(CstI 2, Sub(Var "v", Add(Var "w", Var "z")))
+    let aexpr3 = Add(Var "x", Add(Var "y", Add(Var "z", Var "v")))
+    
+    // Exercise 1.2 (iii)
+    let rec fmt e : string =
+        match e with
+        | CstI x -> string x
+        | Var x -> x
+        | Add (e1, e2) -> "(" + (fmt e1) + " + " + (fmt e2) + ")"
+        | Mul (e1, e2) -> "(" + (fmt e1) + " * " + (fmt e2) + ")"
+        | Sub (e1, e2) -> "(" + (fmt e1) + " - " + (fmt e2) + ")"
+        
+    // Exercise 1.2 (iv)
+    let rec simplify e : aexpr =
+        match e with
+        | CstI x -> CstI x
+        | Var x -> Var x
+        | Add(CstI 0, x) -> simplify x
+        | Add(x, CstI 0) -> simplify x
+        | Sub(x, CstI 0) -> simplify x
+        | Mul(CstI 1, x) -> simplify x
+        | Mul(x, CstI 1) -> simplify x
+        | Mul(CstI 0, _) -> CstI 0
+        | Mul(_, CstI 0) -> CstI 0
+        | Sub(e1, e2) when e1=e2 -> CstI 0
+        | Add(e1, e2) -> simplify (Add(simplify e1, simplify e2))
+        | Mul(e1, e2) -> simplify (Mul(simplify e1, simplify e2))
+        | Sub(e1, e2) -> simplify (Sub(simplify e1, simplify e2))
+        
+    let aexpr4 = Mul(Add(CstI 1, CstI 0), Add(Var "x", CstI 0))
+    
+    // Exercise 1.2 (v)
+    let rec diff e v : aexpr =
+        match e with
+        | CstI _ -> CstI 0
+        | Var x when v = x -> CstI 1
+        | Var _ -> CstI 0
+        | Add(e1, e2) -> Add((diff e1 v), (diff e2 v))
+        | Sub(e1, e2) -> Sub((diff e1 v), (diff e2 v))
+        | Mul(e1, e2) -> Add(Mul((diff e1 v), e2), Mul(e1, (diff e2 v)))
+        
+        
+    
+    
+    
