8d88fe9894dab863bd59c712b4f219277333755b
Daniel Millard <dmil@itu.dk>
2024-10-04 13:19:37 +0200

    make Exercise6_1

---
+++ BPRD-05-alcn-dmil-jacp/Fun/Exercise6_1.fs
@@ -0,0 +1,20 @@
+﻿module Exercise6_1
+
+open ParseAndRunHigher;;
+
+let p1 = run (fromString @"let add x = let f y = x+y in f end
+                           in add 2 5 end");;
+
+let p2 = run (fromString @"let add x = let f y = x+y in f end
+                           in let addtwo = add 2
+                               in addtwo 5 end
+                           end");;
+
+let p3 = run (fromString @"let add x = let f y = x+y in f end
+                           in let addtwo = add 2
+                              in let x = 77 in addtwo 5 end
+                              end
+                           end");;
+
+let p4 = run (fromString @"let add x = let f y = x+y in f end
+                           in add 2 end");;
